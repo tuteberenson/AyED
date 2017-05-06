523a8db7cf51d5540c0646a0ecf488dae08737a0
tuteberenson <tuteberenson1998@gmail.com>
2017-05-05 21:56:42 -0300

    .

--- Palindromas/Palindromas.cpp
+++ Palindromas/Palindromas.cpp
@@ -9,16 +9,15 @@ bool esPalindroma(string, string);
 int main(int argc, char *argv[]) 
 {
 	string palabra, palabraNueva;
+	
+	cout << "Ingrese una frase/palabra";
 	getline(cin,palabra);
 	
 	
-	for(int i = palabraSinEspacios(palabra).length(); i >= 0; i--)
+	for(int i =palabra.length()-1; i >= 0; i--)
 	{
-		cout << palabraSinEspacios(palabra)[i] << "\n";
-		palabraNueva += palabraSinEspacios(palabra)[i];
+		palabraNueva += palabra[i];
 	}
-	 
-	 palabraNueva = palabraSinEspacios(palabraNueva);
 	 
 	 
 	if (esPalindroma(palabra,palabraNueva)){
@@ -41,26 +40,18 @@ string palabraSinEspacios(string palabra){
 		  devolver += palabra[i];
 		}
 	}
-	//cout << devolver <<"\n";
 	return devolver;
 }
 bool esPalindroma(string palabraOriginal, string palabraAcomparar)
 {
-//	cout << palabraSinEspacios(palabraOriginal) <<"\n";
-//	cout << palabraSinEspacios(palabraAcomparar) <<"\n";
 	string p1 = palabraSinEspacios(palabraOriginal);
 	string p2 = palabraSinEspacios(palabraAcomparar);
-//	cout <<  (palabraSinEspacios(palabraOriginal) == palabraSinEspacios(palabraAcomparar)) <<"\n";
 	if(p1 == p2)
 	{
-		cout << "Pa" << "\n";
 		return true;
 	}
 	else
 	{
-				cout << "No Pa" << "\n";
 		return false;
 	}
-	
-	//return (palabraSinEspacios(palabraOriginal) == palabraSinEspacios(palabraAcomparar));
 }
